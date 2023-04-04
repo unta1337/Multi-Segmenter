d767c90497567bbd333f4a66a5413f74761cd4a9
unta1337 <51486948+unta1337@users.noreply.github.com>
2023-04-04 10:08:04 +0000

    Deploying to gh-pages from @ unta1337/Multi-Segmentor@7fc3ba46544da050b18a455027879bc9f11dc17e 🚀

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['cmake_5fminimum_5frequired_0',['cmake_minimum_required',['../_c_make_lists_8txt.html#a4a1bf6e938e6a7433f65bf83e97ae3d6',1,'CMakeLists.txt']]],
+  ['cmake_5fminimum_5frequired_0',['cmake_minimum_required',['../_c_make_lists_8txt.html#ac62e3af94f6f9cb44f49f78ff0a81440',1,'CMakeLists.txt']]],
   ['cmakelists_2etxt_1',['CMakeLists.txt',['../_c_make_lists_8txt.html',1,'']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cmake_5fminimum_5frequired_25',['cmake_minimum_required',['../_c_make_lists_8txt.html#a4a1bf6e938e6a7433f65bf83e97ae3d6',1,'CMakeLists.txt']]]
+  ['cmake_5fminimum_5frequired_25',['cmake_minimum_required',['../_c_make_lists_8txt.html#ac62e3af94f6f9cb44f49f78ff0a81440',1,'CMakeLists.txt']]]
 ];
