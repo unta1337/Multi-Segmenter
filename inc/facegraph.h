6c43c0aee713c23c77abdfdd44418add0b081bc0
refracta <58779799+refracta@users.noreply.github.com>
2023-04-15 02:14:01 +0900

    facegraph 내부 함수 분리

---
+++ inc/facegraph.h
@@ -0,0 +1,24 @@
+﻿#ifndef __FACEGRAPH_H
+#define __FACEGRAPH_H
+
+#include "facegraphutils.h"
+#include "glm/vec3.hpp"
+#include "glm/vector_relational.hpp"
+#include "triangle.h"
+#include "trianglemesh.hpp"
+#include <stack>
+#include <unordered_map>
+#include <vector>
+
+namespace FaceGraph {
+class FaceGraph {
+  public:
+    std::vector<std::vector<int>> adj_list;
+    std::vector<Triangle>* ref_vector;
+
+    FaceGraph(std::vector<Triangle>* list);
+    std::vector<std::vector<Triangle>> check_connected();
+    void traverse_dfs(std::vector<int>* visit, int start_vert, int count);
+};
+} // namespace FaceGraph
+#endif
