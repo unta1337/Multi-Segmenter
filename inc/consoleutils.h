b1a2ec2208648bd117f46eb31085d1ab0400d44a
refracta <58779799+refracta@users.noreply.github.com>
2023-04-15 06:48:14 +0900

    Windows 환경에서 한글 출력 개선

---
+++ inc/consoleutils.h
@@ -0,0 +1,15 @@
+﻿#ifndef __CONSOLEUTILS_H
+#define __CONSOLEUTILS_H
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#define INIT_CONSOLE() initConsole()
+void initConsole() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+}
+
+#endif
