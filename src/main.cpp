4fe74a1493420a3625a4faaac3cf0d5f3c3dff30
refracta <58779799+refracta@users.noreply.github.com>
2023-04-16 08:09:22 +0900

    경로 입력 로직 개선 및 출력 경로 재조정

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+    INIT_CONSOLE();
+
     if (argc != 2) {
         std::cout << "Usage:\n";
         std::cout << "    " << argv[0] << " {obj_file_path}\n";
@@ -13,9 +15,20 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::string file_path = argv[1];
+    std::string file_path;
+    for (int i = 1; i < argc; ++i) {
+        file_path += std::string(argv[i]) + " ";
+    }
 
-    INIT_CONSOLE();
+    file_path.pop_back();
+    std::replace(file_path.begin(), file_path.end(), '\\', '/');
+    size_t separator_next_index = file_path.find_last_of('/') + 1;
+    std::string folder_path = file_path.substr(0, separator_next_index);
+    std::string filename = file_path.substr(separator_next_index, file_path.length());
+    size_t extension_index = filename.find_last_of('.');
+    if (extension_index != std::string::npos) {
+        filename = filename.substr(0, extension_index);
+    }
 
     Model model;
     model.read_obj(file_path);
@@ -36,10 +49,10 @@ int main(int argc, char* argv[]) {
     }
 
     // 구분된 부분별 .obj 저장. 각 부분별 명칭으로 저장됨. (i.e., cube_seg0.obj)
-    write_obj(seg, "", false);
+    write_obj(seg, folder_path, false);
 
     // 한꺼번에 .obj 저장.
-    write_obj(seg, "Segmented_cube", true);
+    write_obj(seg, folder_path + "Segmented_" + filename, true);
 
     return 0;
 }
