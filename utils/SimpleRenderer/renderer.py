5a6791b55c65a07bbfc5bcfaff714c0c8616e076
refracta <58779799+refracta@users.noreply.github.com>
2023-04-16 17:34:53 +0900

    자동 assets 빌드 시스템 추가 (#19)
    * SimpleRenderer 추가
    
    * assets 빌드 및 확인용 GitHub Action 추가
    
    * python 버전 변경 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 절대 경로를 사용하도록 수정
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * Linux 환경 빌드 지원 (#include <format> 제거)
    
    * 스케일링 인수 추가 및 기본 인수 설정 변경
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 빌드 테스트
    
    * 자동 빌드 모델 목록에서 제외
    
    * 검색 깊이 조절
    
    * scale 100 테스트
    
    * scale 75 테스트
    
    * scale 50 테스트
    
    * scale 25 테스트
    
    * frame 15 테스트
    
    * frame 30 테스트
    
    * 비디오 빌드 액션 추가
    
    * assets 빌드 결과 (Segmented 파일) 확인 액션 추가
    
    * complex_models 결과 추가

---
+++ utils/SimpleRenderer/renderer.py
@@ -0,0 +1,110 @@
+import bpy
+import os
+import math
+import shutil
+import argparse
+import imageio.v2 as imageio
+
+# parser 객체 생성
+parser = argparse.ArgumentParser()
+
+# 인자 추가
+parser.add_argument("--fps", type=int, default=10, help="FPS argument")
+parser.add_argument("--frame", type=int, default=60, help="Frame argument")
+parser.add_argument("--quality", type=int, default=6, help="Quality argument")
+parser.add_argument("--align", action="store_true", help="Align argument")
+parser.add_argument("--videoonly", action="store_true", help="Video only argument")
+parser.add_argument("--scale", type=int, default=100, help="Scale argument")
+parser.add_argument("filepath", help="Filepath argument")
+
+# 인자 파싱
+args = parser.parse_args()
+
+fps = args.fps
+frame = args.frame
+quality = args.quality
+align = args.align
+videoonly = args.videoonly
+filepath = os.path.abspath(args.filepath)
+scale = args.scale
+
+dir_path = os.path.dirname(filepath)
+file_name, file_ext = os.path.splitext(os.path.basename(filepath))
+
+# 작업 디렉토리 설정
+directory = dir_path
+os.chdir(directory)
+
+# 출력 디렉토리 설정
+output_dir = os.path.abspath("./frames/")
+os.makedirs(output_dir, exist_ok=True) # 디렉토리가 없으면 생성
+
+# 모든 오브젝트 가져오기
+objects = bpy.context.selected_objects
+
+# Cube 객체 제거하기
+for obj in objects:
+    if obj.name == "Cube":
+        bpy.data.objects.remove(obj, do_unlink=True)
+
+# obj 파일 가져 오기
+bpy.ops.import_scene.obj(filepath=filepath)
+
+# 뷰어 설정 및 렌더링
+bpy.context.scene.render.engine = 'BLENDER_EEVEE'
+bpy.context.scene.render.image_settings.file_format = 'PNG'
+bpy.context.scene.render.resolution_percentage = scale
+
+# 모든 오브젝트 가져오기
+objects = bpy.context.selected_objects
+
+# 모든 오브젝트를 Collection에 넣기
+collection_name = "AllObjects"
+if not bpy.data.collections.get(collection_name):
+    bpy.data.collections.new(collection_name)
+bpy.context.scene.collection.children.link(bpy.data.collections[collection_name])
+for obj in objects:
+    bpy.data.collections[collection_name].objects.link(obj)
+
+# 모든 오브젝트를 선택
+for obj in bpy.context.scene.objects:
+    obj.select_set(True)
+
+# 카메라를 선택한 상태에서 선택한 오브젝트가 모두 보이도록 이동
+bpy.ops.view3d.camera_to_view_selected()
+
+# 회전 애니메이션 만들기
+axes = ['x', 'y', 'z'];
+for axis in range(3):
+    for obj in objects:
+        obj.rotation_euler = (0, 0, 0)
+
+    for i in range(frame):
+        filepath = os.path.join(output_dir, f"{file_name}_{axes[axis]}_{i:04d}.png")
+        bpy.context.scene.render.filepath = filepath
+
+        # 모든 오브젝트 회전 설정
+        for obj in objects:
+            obj.rotation_euler[axis] = i * 2 * math.pi / frame # z축 기준으로 회전
+
+        if align:
+            # 모든 오브젝트를 선택
+            for obj in bpy.context.scene.objects:
+                obj.select_set(True)
+
+            # 카메라를 선택한 상태에서 선택한 오브젝트가 모두 보이도록 이동
+            bpy.ops.view3d.camera_to_view_selected()
+
+        # 렌더링 실행
+        bpy.ops.render.render(write_still=True)
+
+for axis in axes:
+    writer = imageio.get_writer(f"{dir_path}/{file_name}_{axis}_axis.mp4", fps=fps, quality=quality)
+    for i in range(frame):
+        file = os.path.join(output_dir, f"{file_name}_{axis}_{i:04d}.png")
+        im = imageio.imread(file)
+        writer.append_data(im)
+    writer.close()
+
+if videoonly:
+    shutil.rmtree(output_dir)
