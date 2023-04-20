86ae3883e3a45ff6b31bf09287ea7c28a4c274f0
refracta <58779799+refracta@users.noreply.github.com>
2023-04-21 08:19:08 +0900

    임시 수정 적용

--- utils/Executor/converter.ps1
+++ utils/Executor/converter.ps1
@@ -1,19 +1,19 @@
 $folderPath = "assets/tests"
-$exe = $args[0]
-Get-ChildItem -Path $folderPath -Include Segmented_* -Recurse | Remove-Item
-Remove-Item report.txt -ErrorAction SilentlyContinue
-$files = Get-ChildItem -Path $folderPath -Filter *.obj
+#$exe = $args[0]
+#Get-ChildItem -Path $folderPath -Include Segmented_* -Recurse | Remove-Item
+Remove-Item report.csv -ErrorAction SilentlyContinue
+#$files = Get-ChildItem -Path $folderPath -Filter *.obj
 
-$tolerances = @(15, 30, 60, 90, 120, 150, 180)
+#$tolerances = @(15, 30, 60, 90, 120, 150, 180)
 
-foreach ($file in $files) {
-    foreach ($tolerance in $tolerances) {
-        echo serial $tolerance $file.Name
-        & $exe serial $tolerance $file
-        echo parallel $tolerance $file.Name
-        & $exe parallel $tolerance $file
-    }
-}
+#foreach ($file in $files) {
+#    foreach ($tolerance in $tolerances) {
+#        echo serial $tolerance $file.Name
+#        & $exe serial $tolerance $file
+#        echo parallel $tolerance $file.Name
+#        & $exe parallel $tolerance $file
+#    }
+#}
 
 $serials = Get-ChildItem -Path $folderPath -Filter *serial*.txt
 $parallels = Get-ChildItem -Path $folderPath -Filter *parallel*.txt
@@ -21,7 +21,6 @@ $parallels = Get-ChildItem -Path $folderPath -Filter *parallel*.txt
 for ($i=0; $i -lt $serials.Count; $i++) {
     $serial = $serials[$i]
     $parallel = $parallels[$i]
-    echo $parallel.Name >> report.txt
-    node utils/LogTool/converter.js $serial
-    node utils/LogTool/index.js $parallel
+    node utils/LogTool/converter.js $serial report.csv
+    node utils/LogTool/converter.js $parallel report.csv
 }
