6d74097593e5014d76e38c402778e4a9ae129c93
refracta <58779799+refracta@users.noreply.github.com>
2023-06-10 01:52:11 +0900

    args 테스트

--- utils/LogTool/parser.js
+++ utils/LogTool/parser.js
@@ -78,8 +78,8 @@ const args = parser.parse_args();
         version
     };
     console.log(JSON.stringify(data, null, 4));
-    console.log(args);
-    if (args.gsemail && args.gskey && args.gsdoc) {
+    // if (args.gsemail && args.gskey && args.gsdoc) {
+    if (args.gsemail && args.gskey) { args.gsdoc = '1fK7uAQMxsjX4UtLPL1mI0lYvO0wkGfea600J0O4dFb4';
         const doc = new GoogleSpreadsheet(args.gsdoc);
         await doc.useServiceAccountAuth({
             client_email: args.gsemail,
