324e87c67c9d6f066aff37bf74fa881a4a2e650a
refracta <58779799+refracta@users.noreply.github.com>
2023-06-10 01:29:58 +0900

    경로 수정

--- utils/Executor/record.sh
+++ utils/Executor/record.sh
@@ -6,13 +6,15 @@ key=$3
 doc=$4
 modes=("serial" "parallel" "cuda")
 tolerances=("15.0" "30.0")
-obj_files=(assets/tests/*.obj) # obj 파일 경로에 맞게 수정해주세요
+obj_files=(assets/tests/*.obj)
 
 for mode in "${modes[@]}"; do
   for tolerance in "${tolerances[@]}"; do
     for obj_file in "${obj_files[@]}"; do
       $execute "$mode" "$tolerance" "$obj_file"
-      segmented_file="assets/tests/Segmented_${mode}_${tolerance}_${obj_file}.txt"
+      filename=$(basename -- "$obj_file")
+      filename="${filename%.*}"
+      segmented_file="assets/tests/Segmented_${mode}_${tolerance}_${filename}.txt"
       node utils/LogTool/parser.js --f "$segmented_file" -e "$email" -k "$key" -d "$doc"
     done
   done
