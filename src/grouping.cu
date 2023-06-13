2f00663b44e3fd59dc3223de409934184c28a82f
refracta <58779799+refracta@users.noreply.github.com>
2023-06-14 06:07:54 +0900

    빌드 테스트2

--- src/grouping.cu
+++ src/grouping.cu
@@ -81,26 +81,6 @@ std::unordered_map<unsigned int, std::vector<Triangle>> kernelCall(TriangleMesh*
     cudaStream_t stream;
     cudaStreamCreate(&stream);
 
-    DS_timer timer(1);
-    timer.onTimer(0);
-
-    Triangle* dVertexAlign;
-    Pair* dGroup;
-    Triangle* TriangleList = (Triangle*)malloc(sizeof(Triangle) * mesh->index.size());
-    Pair* group = (Pair*)malloc(sizeof(Pair) * mesh->index.size());
-#pragma omp parallel for
-    for (int i = 0; i < mesh->index.size(); i++) {
-        TriangleList[i].vertex[0] = mesh->vertex[mesh->index[i].x];
-        TriangleList[i].vertex[1] = mesh->vertex[mesh->index[i].y];
-        TriangleList[i].vertex[2] = mesh->vertex[mesh->index[i].z];
-    }
-    cudaMallocAsync(&dGroup, sizeof(Pair) * mesh->index.size(), stream);
-    cudaMalloc(&dVertexAlign, sizeof(Triangle) * mesh->index.size());
-    cudaMemcpy(dVertexAlign, TriangleList, sizeof(Triangle) * mesh->index.size(), cudaMemcpyHostToDevice);
-
-#define BLOCK_SIZE 512
-    grouping<<<ceil((float)mesh->index.size() / BLOCK_SIZE), BLOCK_SIZE>>>(dVertexAlign, dGroup, mesh->index.size(),
-                                                                           tolerance);
     std::unordered_map<unsigned int, std::vector<Triangle>> normal_triangle_list_map;
 
     return normal_triangle_list_map;
